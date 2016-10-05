559586bdfebdd8cf55adf3ff807e461baeb6cfee
JanssenRodrigues <faker.rdc@hotmail.com>
2016-10-05 20:31:39 -0300

    Index Html Adjusts

--- index.php
+++ index.php
@@ -1,29 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Facebook Login JavaScript Example</title>
-<meta charset="UTF-8">
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script type="text/javascript" src="js/main.js" ></script>
-<link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Facebook Login JavaScript Example</title>
+  <meta charset="UTF-8">
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script type="text/javascript" src="js/main.js" ></script>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<style>
-    
-</style>
-<div id="fb-root"></div>
+  <div id="fb-root"></div>
+  <!-- <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="true" data-auto-logout-link="true"></div> -->
+  <fb:login-button scope="public_profile,email" data-auto-logout-link="true" onlogin="checkLoginState();"></fb:login-button>
 
-<!-- <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="true" data-auto-logout-link="true"></div> -->
-<fb:login-button scope="public_profile,email" data-auto-logout-link="true" onlogin="checkLoginState();">
-</fb:login-button>
-
-<div id="status">
-</div>
-<h1>CONFIRMADOS</h1>
-<div id="callAPI">
-
-    
-</div>
+  <div id="status"></div>
+  <h1>CONFIRMADOS</h1>
+  <div id="callAPI"></div>
 
 </body>
 </html>
